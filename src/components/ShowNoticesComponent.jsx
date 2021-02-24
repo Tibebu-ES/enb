2a1e919475d1e40ffc2f76fac44213a621345a28
Tibebu-ES <tibebuenyew@gmail.com>
2021-02-24 18:37:32 +0300

    Project structure created; Bootstrap installed; Header and footer components created

---
+++ src/components/ShowNoticesComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShowNoticesComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Show Notices Page!</h1>
+            </div>
+        );
+    }
+}
+
+export default ShowNoticesComponent;
